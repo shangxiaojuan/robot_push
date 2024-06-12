4c17b797aad43c0fe0c093489224b49ab6ca999c
shangxiaojuan <1390115692@qq.com>
2024-06-12 15:45:23 +0800

    third commit

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ def send_text(webhook, content, mentioned_list=None, mentioned_mobile_list=None)
     data2 = {
             "msgtype": "markdown",  # 消息类型，此时固定为markdown
             "markdown": {
-                "content": "# **【i检验研发测试进度】**<font color=\"warning\">**xxx例**</font>\n" +  # 标题 （支持1至6级标题，注意#与文字中间要有空格）
-                           "#### **bug进度**\n" +  # 加粗：**需要加粗的字**
-                           "> 今日新增bug个数：<font color=\"info\">XXX</font> \n" +  # 引用：> 需要引用的文字
-                           "> ：<font color=\"warning\">xxx个</font> \n" # 字体颜色(只支持3种内置颜色)  # 绿色：info、灰色：comment、橙红：warning
+                "content": "# **【i检验研发测试进度】**\n" +  # 标题 （支持1至6级标题，注意#与文字中间要有空格）
+                           "### **bug总数：**<font color=\"info\">10个</font>\n" +  # 加粗：**需要加粗的字**
+                           "> 今日新增bug个数：<font color=\"info\">5个</font> \n" +  # 引用：> 需要引用的文字
+                           "> 未解决bug：<font color=\"warning\">4个</font> \n" # 字体颜色(只支持3种内置颜色)  # 绿色：info、灰色：comment、橙红：warning
             }
     }
     data = json.dumps(data2)
